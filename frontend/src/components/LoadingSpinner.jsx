6c600c967b7d98a117fd4416263aaabf54f47548
kensondesu <126876642+kensondesu@users.noreply.github.com>
2026-02-23 17:39:54 +0000

    Revert "feat: modernize Oracle WebForms to React + FastAPI + PostgreSQL on Azure Container Apps"

--- frontend/src/components/LoadingSpinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center py-12">
-      <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-primary-600"></div>
-    </div>
-  )
-}
